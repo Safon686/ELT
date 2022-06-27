1d840523822a97a7c70b1deab472d950f53b2608
Safon686 <safon686@gmail.com>
2022-06-27 19:01:01 +0700

    final ver

--- phonebook/phonebook.c
+++ phonebook/phonebook.c
@@ -1,66 +1,123 @@
 #include "st.h"
 
-
 int main() {
-    int step = 1;
-    int  var = 0;
-    int amount = 0;
-    char name[20];
-    int number=000000;
-    FILE *fd;
-    fd=fopen("cards.txt", "r+b");
-        if (fd == NULL) {
-                printf("File not found \n");
-        }
-        else {
-                printf("Open file \n");
-                fread(&book, 1000, sizeof(book[1000]), fd);
-                printf("Size of book %ld\n", sizeof(*book));
-                printf("Phonebook load \n");
-        }
-
-    while (step==1) {
+  int step = 1;
+  int var = 0;
+  int amount = 0;
+  char name[20];
+  char search_name[20];
+  int number = 0;
+  int card_search_by_name = -1;
+  int search_number=-1;
+  int number_search=000000;
+  int number_del=999999;
+  int del=999999;
+  FILE *fd;
+  fd = fopen("cards.txt", "r");
+  if (fd == NULL) {
+    printf("File not found\n");
+    FILE *create;
+    create = fopen("cards.txt", "wb");
+    fclose(create);
+    printf("New file create\n");
+    fd = fopen("cards.txt", "r");
+    fread(&book, 1, sizeof(book), fd);
+    fclose(fd);
+    printf("size of Book %ld byte\n", sizeof(book));
+  } else {
+    printf("Open file\n");
+    fread(&book, 1, sizeof(book), fd);
+    fclose(fd);
+    printf("Phonebook load\n");
+  }
+  FILE *rd;
+  rd = fopen("numbers.txt", "r");
+  if (rd == NULL) {
+    printf("Book is clear, create first file card\n");
+    FILE *nw;
+    nw = fopen("numbers.txt", "wb");
+    fclose(nw);
+    nw=fopen("numbers.txt", "w");
+    fwrite(&lastcard, 1, sizeof(lastcard), nw);
+    printf("write in new file %d\n", lastcard);
+    fclose(nw);
+  } else {
+    fread(&lastcard, 1, sizeof(lastcard), rd);
+    fclose(rd);
+  }
+  
+  while (step == 1) {
 
     printf("1. Create a new subscriber card \n");
     printf("2. Search by phone number \n");
     printf("3. Search by last name \n");
-    printf("4. Open Phonebook from file\n");
-    printf("5. Save Phonebook \n");
-    printf("6. Delete card by phone number \n");
-    printf("7. Exit program \n\n");
+    printf("4. Save change \n");
+    printf("5. Delete card by phone number \n");
+    printf("6. Exit program \n\n");
+   /* for (int i=0; i<10; i++) {
+    printf("%s is %d\n", book[i].name, book[i].number);
+    }
+    */     //open first 10 card under main
     scanf("%d", &var);
     switch (var) {
-        case 1:
-		printf("Enter Last name\n\n");
-		scanf("%s", name);
-		printf("Enter number\n\n");
-		scanf("%d", &number);
-		add_card(name,number);
-		printf("Card create!\n\n");
-        break;
-        case 2:
-       		printf("%s: %d\n", book[0].name, book[0].number); 
-        break;    
-        case 3:
-                printf("%s: %d\n", book[1].name, book[1].number);
-        break;
-        case 4:
-		
-	break;
-        case 5:
-        	fwrite(&book,sizeof(book)*lastcard, 1, fd);
-                printf("Phonebook SAVE!\n\n");
-        break;
-        case 6:
-        
-        break;
+    case 1:
+      printf("Enter Last name\n\n");
+      scanf("%s", name);
+      printf("Enter number\n\n");
+      scanf("%d", &number);
+      add_card(name, number);
+      printf("Card create!\n\n");
+      break;
+    case 2:
+	printf("Enter Search numbers\n");
+	scanf(" %d", &number_search);
+	search_number=search_card_by_number(number_search);
+	if (search_number==-1) {
+	printf("Number not found\n");
+	}
+	else {
+	printf("Name from %d is %s\n\n", number_search, book[search_number].name);
+	}
+      break;
+    case 3:
+      printf("Enter Last name for search\n");
+      scanf(" %s", search_name);
+      card_search_by_name = search_card_by_name(search_name);
+      if (card_search_by_name == -1) {
+        printf("Name not found\n");
+      } else {
+        printf("Phone number of %s is %d\n\n", search_name,
+               book[card_search_by_name].number);
+      }
 
-	case 7:
-		step++;
-	break;
+      break;
+   
+    case 4:
+      fd=fopen("cards.txt", "w");
+      fwrite(&book, sizeof(book), 1, fd);
+      fclose(fd);
+      printf("Phonebook SAVE!\n\n");
+      break;
+    case 5:
+	printf("Enter number to delete\n");
+	scanf("%d", &del);
+	number_del=delete_card_by_number(del);
+     	if (number_del==-1) {
+		printf("Number for delete not faund\n\n");
+	} else {
+	for (int i=number_del; i<1000; i++) {
+	strcpy(book[i].name, book[i+1].name);
+	book[i].number=book[i+1].number;
+	}
+	printf("Card delete\n");
+	}
+       	break;
+
+    case 6:
+      step++;
+      break;
     }
-}
+  }
 
-fclose(fd);
-printf("File close \n");
+  printf("File close \n");
 }
--- phonebook/st.h
+++ phonebook/st.h
@@ -1,9 +1,9 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 struct phonebook {
-char name[20];
-int number;
+  char name[20];
+  int number;
 };
 
 struct phonebook book[1000];
@@ -13,18 +13,49 @@ int lastcard = -1;
  * returns -1 if card was not added, because book is full
  */
 int add_card(const char *name, int number) {
-	if (lastcard == 999) return -1;
-	lastcard++;
-        strcpy(book[lastcard].name,name);
-	book[lastcard].number=number;
+  if (lastcard == 999) {
+    return -1;
+  }
+  FILE *nd;
+  nd = fopen("numbers.txt", "r");
+  fread(&lastcard, 1, sizeof(lastcard), nd);
+  printf("Lastcard from file %d\n", lastcard);
+  fclose(nd);
+  lastcard++;
+  nd=fopen("numbers.txt", "w");
+  fwrite(&lastcard, 1,sizeof(lastcard), nd);
+  fclose(nd);
+  strcpy(book[lastcard].name, name);
+  book[lastcard].number = number;
 }
 
-int search_card_by_name(const char *name) {
+int search_card_by_name(const char *name_search) {
+  for (int i = 0; i < 1000; i++) {
+    if (strcmp(book[i].name, name_search) == 0) {
+      return i;
+    }
+   }
+  
+  return -1;
 }
 
-int search_card_by_number(int number) {
+
+int search_card_by_number(int number_search) {
+  for (int i=0; i<1000; i++) {
+    if (book[i].number == number_search) {
+    return i;
+    }
+  }
+  return -1;
 }
 
-void delete_card_by_name(const char *name) {
-}
 
+int delete_card_by_number(int delete_card ) {
+for (int i=0; i<1000; i++) {
+    if (book[i].number == delete_card) {
+    return i;
+    }
+  }
+  return -1;
+
+}
